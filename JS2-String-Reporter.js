765d708187d5197e411664a87c6593c5b17f1904
Student <kitty8mars@gmail.com>
2015-06-23 10:49:02 -0600

    Soon

---
+++ JS2-String-Reporter.js
@@ -0,0 +1,12 @@
+
+
+
+
+
+// window.prompt("How old are you?");
+// var age = prompt("How old are you?")
+// console.log(age);
+
+var age = prompt("How old are you?")
+ 
+   console.log(age.length)
