14317e2f5e730b1eae373e596efb7d0b2b2efead
Student <kitty8mars@gmail.com>
2015-06-23 11:39:36 -0600

    whew

--- JS2-String-Reporter.js
+++ JS2-String-Reporter.js
@@ -7,6 +7,28 @@
 // var age = prompt("How old are you?")
 // console.log(age);
 
-var age = prompt("How old are you?")
+// var age = prompt("How old are you?")
+//  	console.log(age.length)
+
  
-   console.log(age.length)
+ 	// var dessert = prompt("What is your favorite dessert?");	
+ 	// console.log(dessert.charAt(3))
+
+// We are not assigning the prompt value to a variable
+// Prompt will evaluate to whatever is typed in the box
+
+
+ // console.log(dessert.charAt(3))
+
+ // var dessert = prompt("What is your favorite dessert?");	
+ // 	console.log(dessert.toLowerCase())
+
+ 
+ // var dessert = prompt("What is your favorite dessert?");	
+ // 	console.log(dessert + " is my favorite dessert")
+
+var dessert = prompt("What is your favorite dessert?");	
+ 	console.log(dessert.substring(2, 5));
+
+ 	// THE LAST ARGUMENT IS NON-INCLUSIVE;  IF YOU PUT IN 4, IT 
+ 	// WILL END AT INDEX 3
